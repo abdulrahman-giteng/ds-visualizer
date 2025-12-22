9505d7158848b6051c1d4e5ff34c8b11d7d0f233
Abdulrahman Zrein <abdulrahmangit123@gmail.com>
2025-12-22 14:29:18 -0500

    Implement and test singly linked list core

---
+++ c_src/test_list.c
@@ -0,0 +1,36 @@
+#include "ds.h"
+#include <stdlib.h> // malloc, free
+#include <assert.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Creating list...\n");
+    list_t *list = list_create();
+    assert(list != NULL);
+
+    printf("Pushing elements to front...\n");
+    list_push_front(list, 10);
+    list_push_front(list, 20);
+    list_push_front(list, 30);
+
+    assert(list_size(list) == 3);
+
+    printf("Popping elements from front...\n");
+    int value;
+    while (list_pop_front(list, &value))
+    {
+        printf("Popped: %d\n", value);
+    }
+
+    assert(list_size(list) == 0);
+
+    printf("Testing pop on empty list...\n");
+    assert(list_pop_front(list, &value) == 0);
+
+    printf("Destroying list...\n");
+    list_destroy(list);
+
+    printf("All tests passed!\n");
+    return 0;
+}
